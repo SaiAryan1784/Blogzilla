e5a8787209276f7a011a2e1736fe4065eef923a1
aaanishaaa <anishaworks21@gmail.com>
2024-05-27 15:31:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,53 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import { useEffect, useState } from "react";
 import { auth } from "./firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import Login from "./components/auth/Login";
 import SignUp from "./components/auth/Signup";
-import Home from "./components/Home";  // Assuming you have a Home component
+import Home from "./components/Home";
 import AuthDetails from "./components/AuthDetails";
 
 const App = () => {
-    const [authUser, setAuthUser] = useState(null);
+  const [authUser, setAuthUser] = useState(null);
 
-    useEffect(() => {
-        const listen = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setAuthUser(user);
-            } else {
-                setAuthUser(null);
-            }
-        });
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user);
+      } else {
+        setAuthUser(null);
+      }
+    });
 
-        return () => {
-            listen();
-        };
-    }, []);
+    return () => {
+      listen();
+    };
+  }, []);
 
-    return (
-        <Router>
-            <Routes>
-                <Route
-                    path="/"
-                    element={authUser ? <Navigate to="/home" /> : <Navigate to="/login" />}
-                />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route
-                    path="/home"
-                    element={authUser ? <Home /> : <Navigate to="/login" />}
-                />
-            </Routes>
-            <AuthDetails />
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            authUser ? <Navigate to="/home" /> : <Navigate to="/login" />
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/home"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+      </Routes>
+      <AuthDetails />
+    </Router>
+  );
 };
 
 export default App;
